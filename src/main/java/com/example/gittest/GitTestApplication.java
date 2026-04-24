37873e22e90a5dc64a4095e65a862b7c14286954
Minju Kang <brio0106@gmail.com>
2026-04-24 11:24:03 +0900

    1차 릴리즈

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -10,4 +10,15 @@ public class GitTestApplication {
         SpringApplication.run(GitTestApplication.class, args);
     }
 
+    public void sayHello() {
+        System.out.println("Hello from Demo1Application!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public String getStatus() {
+        return "Application is running";
+    }
 }
